fec6aeefa14d299774c55ec7aa7a252a636146d4
mec_test_1 <mec_test_1@hotmail.com>
2016-12-08 12:29:54 +0800

    implement the pagination function.

--- app/pro-angular/controllers/productListControllers.js
+++ app/pro-angular/controllers/productListControllers.js
@@ -1,13 +1,27 @@
 
 angular.module('sportsStore')
-		.constant('productListActiveClass', 'btn-primary')
-		.controller('productListCtrl', ['$scope', '$filter', 'productListActiveClass', 
-					function($scope, $filter, productListActiveClass){
+//		.constant('productListActiveClass', 'btn-primary')
+//		.constant('productListPageCount', 3)
+		.constant('productListConfig', {
+			productListActiveClass: 'btn-primary',
+			productListPageCount: 3
+//		}).controller('productListCtrl', ['$scope', '$filter', 'productListActiveClass', 'productListPageCount', 
+//					function($scope, $filter, productListActiveClass, productListPageCount){
+		}).controller('productListCtrl', ['$scope', '$filter', 'productListConfig', 
+					function($scope, $filter, config){
 			var selectedCategory = null;
+			
+			$scope.selectedPage = 1;
+			$scope.pageSize = config.productListPageCount;
 	
 			$scope.selectCategory = function(newCategory){
 				selectedCategory = newCategory;
+				$scope.selectedPage = 1;
 			};
+			
+			$scope.selectPage = function(newPage){
+				$scope.selectedPage = newPage;
+			}
 			
 			$scope.categoryFilterFn = function(product){
 				return !selectedCategory || angular.equals(selectedCategory, product.category);
@@ -16,8 +30,47 @@ angular.module('sportsStore')
 			$scope.getCategoryClass = function(category){
 				var retval = '';
 				if(angular.equals(selectedCategory, category)){
-					retval = productListActiveClass;
+					retval = config.productListActiveClass;
 				}
 				return retval;
 			};
-		}]);
+			
+			$scope.getPageClass = function(page){
+				var retval = '';
+				if(angular.equals(page, $scope.selectedPage)){
+					retval = config.productListActiveClass;
+				}
+				return retval;
+			};
+		}]).filter('range', ['$filter', function($filter){
+			var limitTo = $filter('limitTo');
+			return function (data, page, size){
+				var retval = [];
+				if(angular.isArray(data) && angular.isNumber(page) && angular.isNumber(size)){
+					var start_index = (page - 1 ) * size;
+					if(data.length < start_index){
+						//return [];
+						
+					}else{
+						//return limitTo(size, index);
+						//retval = limitTo(data.splice(start_index), size);
+						retval = limitTo(data, size, start_index);
+					}
+				}else{
+					retval = data;
+				}
+				return retval;
+			};
+		}]).filter('pageCount', function(){
+			return function(data, size){
+				var retval = [];
+				if(angular.isArray(data) && angular.isNumber(size)){
+					for(var i = 0; i < Math.ceil(data.length / size); ++i){
+						retval.push(i);
+					}
+				}else{
+					retval = data;
+				}
+				return retval;
+			};
+		});

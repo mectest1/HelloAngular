1cb06b55ba4a667000922dc95e1f03ade6acddf8
mec_test_1 <mec_test_1@hotmail.com>
2016-12-08 12:37:44 +0800

    send ajax requests through $http service;

--- app/pro-angular/app.js
+++ app/pro-angular/app.js
@@ -1,15 +1,24 @@
 
 (function(){
 	angular.module('sportsStore', ['customFilters'])
-			.controller('sportsStoreCtrl', ['$scope', function($scope){
-				$scope.data = {
-					products: [
-						{name: 'Product #1', description: 'A poroduct', category: 'Category #1', price: 100},
-						{name: 'Product #2', description: 'A product', category: 'Category #1', price: 110},
-						{name: 'Product #3', description: 'A product', category: 'Category #2', price: 210},
-						{name: 'Product #4', description: 'A product', category: 'Category #3', price: 202}
-					]
-				};
+			.constant('sportsStoreConfig', {
+				dataUrl: 'http://localhost:5500/products'
+			}).controller('sportsStoreCtrl',  ['$scope', 'sportsStoreConfig', '$http',
+				function($scope, config, $http){
+//				$scope.data = {
+//					products: [
+//						{name: 'Product #1', description: 'A poroduct', category: 'Category #1', price: 100},
+//						{name: 'Product #2', description: 'A product', category: 'Category #1', price: 110},
+//						{name: 'Product #3', description: 'A product', category: 'Category #2', price: 210},
+//						{name: 'Product #4', description: 'A product', category: 'Category #3', price: 202}
+//					]
+//				};
+				$scope.data = [];
+				$http.get(config.dataUrl).success(function(data){
+					$scope.data.products = data;
+				}).error(function(error){
+					$scope.data.error = error;
+				})
 				
 				//$scope.data = data;
 				

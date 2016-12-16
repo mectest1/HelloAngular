c38d4e3f1f5ec24e60eade038b69d5a1e6df6678
mec_test_1 <mec_test_1@hotmail.com>
2016-12-16 11:59:17 +0800

    prepare a simple server app (written with express.js)

---
+++ app/WEB-INF/server.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ * Start a simple server on locathost and listen to port 5000;
+ * 
+ * ref: http://expressjs.com/en/4x/api.html
+ * ref2: http://expressjs.com/en/starter/hello-world.html
+ * 
+ * @type Module express|Module express
+ */
+
+'use strict';
+
+let express = require('express');
+let path = require('path');
+let app = express();
+const PORT = 5000;
+
+//app.use(express.static('../pro-angular'));
+let proAngularPath = path.join(__dirname, '../');
+console.log(`serving ${proAngularPath} with express.js, listening on port number ${PORT}`);
+app.use(express.static(proAngularPath));
+
+app.listen(PORT);

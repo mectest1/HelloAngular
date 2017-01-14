7825a4f911298afbeadf5c55b32c8c736515974e
mec_test_1 <mec_test_1@hotmail.com>
2017-01-14 22:26:03 +0800

    Get isolated scope for our directives;

---
+++ app/pro-angular/modules/directive-scopes-app.js
@@ -0,0 +1,21 @@
+define(['angular', 'text!../views/directive-scopes-view.html',
+		'text!../views/directive-isolated-scopes-view.html'], 
+	function(angular, directiveScopesView, isolatedView){
+	return angular.module('exampleApp', [])
+			.directive('scopeDemo', [function(){
+		return {
+			template: directiveScopesView,
+			scope: true
+		};		
+	}]).directive('isolatedScopeDemo', [function(){
+		return {
+			template: isolatedView,
+			scope: {
+				local: '@nameProp'
+			}
+		};
+	}]).controller('scopeCtrl', ['$scope', function($scope){
+		$scope.data = {name: 'Adam'};
+		$scope.city = 'London';
+	}]);
+});

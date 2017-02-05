8f62a2988e512cbace05ee9a86963ce1376a791a
mec_test_1 <mec_test_1@hotmail.com>
2017-02-05 19:25:16 +0800

    Finish the Promise example;

--- app/pro-angular/modules/promise-intro-app.js
+++ app/pro-angular/modules/promise-intro-app.js
@@ -2,20 +2,28 @@ define(['angular'], function(angular){
 	'use strict';
 	return angular.module('exampleApp', [])
 			.directive('promiseWorker', ['$q', function($q){
-				let deferred = $q.defer();
+				//let deferred = $q.defer();
+				let deferred = [$q.defer(), $q.defer()];
+				let promises = deferred.map(d => d.promise);
 				return {
 					link: function(scope, element, attrs){
 						element.find('button').on('click', function(event){
 							let buttonText = event.target.innerText;
+							let buttonGroup = event.target.getAttribute('data-group');
 							if('Abort' === buttonText){
-								deferred.reject(buttonText);
+								//deferred.reject(buttonText);
+								deferred[buttonGroup].reject(buttonText);
 							}else{
-								deferred.resolve(buttonText);
+								//deferred.resolve(buttonText);
+								deferred[buttonGroup].resolve(buttonText);
 							}
 						});
 					},
 					controller: ['$scope', '$element', '$attrs', function($scope, $element, $attrs){
-						this.promise = deferred.promise;
+						//this.promise = deferred.promise;
+						this.promise = $q.all(promises).then((results) => {
+							return results.join();
+						});
 					}]
 				};
 	}]).directive('promiseObserver', [function(){
@@ -23,9 +31,12 @@ define(['angular'], function(angular){
 			require: '^promiseWorker', 
 			link: function(scope, element, attrs, ctrl){
 				ctrl.promise.then((result) => {
-					element.text(result);
+					return 'Success (' + result + ')';
 				}, (reason) => {
-					element.text('Fail (' + reason + ')');
+					//element.text('Fail (' + reason + ')');
+					return 'Fail (' + reason + ')';
+				}).then((result) => {
+					element.text(result);
 				});
 			}
 		};

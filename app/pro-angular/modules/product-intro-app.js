5c9d31db7376db338a87711222b3e8147117322d
mec_test_1 <mec_test_1@hotmail.com>
2017-02-06 15:23:17 +0800

    Get the dummy data ready for the following $resource examples;

--- app/pro-angular/modules/product-intro-app.js
+++ app/pro-angular/modules/product-intro-app.js
@@ -1,6 +1,55 @@
 define(['angular', 'angular-resource'], function(angular){
+	'use strict';
+	
 	return angular.module('exampleApp', ['ngResource'])
-			.controller('defaultCtrl', ['$scope', function($scope){
-					
+		.controller('defaultCtrl', ['$scope', function($scope){
+		$scope.displayMode = 'list';
+		$scope.currentProduct = null;
+		
+		$scope.listProducts = function(){
+			$scope.products = [
+				{id: 0, name: 'Dummy1', category: 'Test', price: 1.25},
+				{id: 1, name: 'Dummy2', category: 'Test', price: 2.25},
+				{id: 2, name: 'Dummy3', category: 'Test', price: 4.25}
+			];
+		};
+		
+		$scope.deleteProduct = function(product){
+			$scope.products.splice($scope.products.indexOf(product), 1);
+		};
+		
+		$scope.createProduct = function(product){
+			$scope.products.push(product);
+			$scope.displayMode = 'list';
+		};
+		
+		$scope.updateProduct = function(product){
+			$scope.products.forEach((item, index, array) => {
+				if(item.id === product.id){
+					array[index] = product;
+				}
+			});
+			$scope.displayMode = 'list';
+		};
+		
+		//----------------------------------------------		
+		$scope.editOrCreateProduct = function(product){
+			$scope.currentProduct = product ? angular.copy(product) : {};
+			$scope.displayMode = 'edit';
+		};
+		
+		$scope.saveEdit = function(product){
+			if(angular.isDefined(product.id)){
+				$scope.updateProduct(product);
+			}else{
+				$scope.createProduct(product);
+			}
+		};
+		
+		$scope.cancelEdit = function(){
+			$scope.currentProduct = {};
+			$scope.displayMode = 'list';
+		};
+		
 	}]);
 });
